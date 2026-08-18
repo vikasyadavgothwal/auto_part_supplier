6af1e990b3e5e1a4eb76858e182a8038bc727211
HARSHKUMAR65 <kabiryadav55555@gmail.com>
2026-08-18 10:39:27 +0530

    Load supplier Firebase config before client scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,12 +28,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Script
+        id="firebase-runtime-config"
+        src="/api/firebase-config.js"
+        strategy="beforeInteractive"
+      />
       <body className="min-h-full flex flex-col">
-        <script
-          id="firebase-runtime-config"
-          src="/api/firebase-config.js"
-          async={false}
-        />
         {children}
       </body>
     </html>
